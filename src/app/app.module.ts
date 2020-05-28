6f5fcf2e5afc05b3269ac594fbe1e8226671e76b
Priti Mainkar <pritimainkar7@gmail.com>
2020-05-28 22:21:30 +0530

    added new package of material,flex,etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, BrowserAnimationsModule, MatToolbarModule, FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
